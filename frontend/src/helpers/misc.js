ed3315493ab87ad3049fe5465eef3d6a0ef7ee10
Sasha Obucina <sasha.obucina@gmail.com>
2020-03-31 14:35:11 -0400

    Add proper prod URLs

--- frontend/src/helpers/misc.js
+++ frontend/src/helpers/misc.js
@@ -4,6 +4,6 @@ function isDev() {
   return '_self' in React.createElement('div');
 }
 
-export const FETCH_URL = isDev() ? "http://localhost:5000/covid19/" : "http://localhost:5000/covid19/";
-export const PREFETCH_URL = isDev() ? "http://localhost:5000/valid-countries" : "http://localhost:5000/valid-countries";
+export const FETCH_URL = isDev() ? "http://localhost:5000/covid19/" : "https://coronatracker-rest.herokuapp.com/covid19/";
+export const PREFETCH_URL = isDev() ? "http://localhost:5000/valid-countries" : "https://coronatracker-rest.herokuapp.com/valid-countries";
 
