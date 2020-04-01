1cab1ce26250a59ba3caf23b5a7f14c260aaeebd
Sasha Obucina <sasha.obucina@gmail.com>
2020-04-01 02:41:01 -0400

    Add permanent alert when unable to fetch data

--- coronatracker/src/components/Alerts/ErrorAlert.js
+++ coronatracker/src/components/Alerts/ErrorAlert.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Snackbar} from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
+import { strings } from "../../helpers/strings";
 
 class ErrorAlert extends Component {
   render() {
     const { open, message, handleClose } = this.props;
+    const onClose = message === strings.fetch ? () => {} : handleClose;
     return (
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{  horizontal: "left", vertical: "bottom"}}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={onClose} anchorOrigin={{  horizontal: "left", vertical: "bottom"}}>
         <Alert severity="error" variant="filled" onClose={handleClose}>
           {message}
         </Alert>
