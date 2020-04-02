c97824b057e7d77838d3cae84cabec8e07ecc287
Sasha Obucina <sasha.obucina@gmail.com>
2020-04-02 16:13:10 -0400

    Add new heroku deployment instructions

--- flask-microservice/heroku-deploy.sh
+++ flask-microservice/heroku-deploy.sh
@@ -5,3 +5,6 @@ heroku container:login
 ./docker-init.sh
 heroku container:push web -a coronatracker-rest
 heroku container:release web -a coronatracker-rest
+
+# open the microservice or use the url above
+heroku open -a coronatracker-rest
