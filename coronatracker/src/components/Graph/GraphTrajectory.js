8a5764a1bed6c7fa1494b69aaff64012db8e0a17
Sasha Obucina <sasha.obucina@gmail.com>
2020-04-05 19:17:44 -0400

    Add greater domain on trajectory graph x-axis

--- coronatracker/src/components/Graph/GraphTrajectory.js
+++ coronatracker/src/components/Graph/GraphTrajectory.js
@@ -3,7 +3,7 @@ import { Tooltip, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Legend, Line
 
 export default function GraphWeekly(props) {
   const { data, scale } = props;
-  const domain = scale === "log" ? [1, 1000000] : [1, 10000];
+  const domain = scale === "log" ? [1, 10000000] : [1, 10000];
   const ticks = [1, 10, 100, 1000, 10000, 100000, 1000000];
 
   return (
