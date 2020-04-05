b11af82f8e6d771eeb48c56e041f23e7ddc0a6a9
Sasha Obucina <sasha.obucina@gmail.com>
2020-04-05 19:29:53 -0400

    Consistent styling for playback speed button group

--- coronatracker/src/components/Buttons/SpeedButtonGroup.js
+++ coronatracker/src/components/Buttons/SpeedButtonGroup.js
@@ -6,11 +6,29 @@ import { makeStyles } from "@material-ui/styles"
 const useStyles = makeStyles({
   root: {
     background: '#3C3F58',
-    color: '#3BBA9C'
+    color: '#3BBA9C',
+    "&:hover": {
+      background: '#3C3F58',
+    }
   },
-  toggleRoot: {
-    background: '#3C3F58',
-    color: '#3BBA9C'
+  button: {
+    background: "#3C3F58",
+    color: "#212121",
+    "&:hover": {
+      background: "#3C3F58",
+      color: "#3BBA9C",
+      fontWeight: "bolder",
+      opacity: 0.8,
+    },
+    "&.Mui-selected": {
+      background: '#3C3F58',
+      color: "#3BBA9C",
+      fontWeight: "bold",
+      "&:hover": {
+        background: "#3C3F58",
+        opacity: 1,
+      },
+    }
   }
 });
 
@@ -32,16 +50,16 @@ export default function SpeedButtonGroup(props) {
         exclusive
         onChange={handleChange}
       >
-        <ToggleButton className={classes.toggleRoot} value={0.25}>
+        <ToggleButton className={classes.button} value={0.25}>
           0.25x
         </ToggleButton>
-        <ToggleButton className={classes.toggleRoot} value={0.5}>
+        <ToggleButton className={classes.button} value={0.5}>
           0.5x
         </ToggleButton>
-        <ToggleButton className={classes.toggleRoot} value={1}>
+        <ToggleButton className={classes.button} value={1}>
           1x
         </ToggleButton>
-        <ToggleButton className={classes.toggleRoot} value={2}>
+        <ToggleButton className={classes.button} value={2}>
           2x
         </ToggleButton>
       </ToggleButtonGroup>
