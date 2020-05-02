250fcc1e6ee3bb6d171882e7d005910f04a80a2c
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-02 03:52:49 -0400

    Refine backup script; add more backup data files

--- flask-microservice/backups/backup.py
+++ flask-microservice/backups/backup.py
@@ -5,6 +5,8 @@ import urllib.request as request
 CONFIRMED_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 DEATHS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
+ABSOLUTE_PATH = "/Users/sashaobucina/repos/coding_projects/coronatracker/flask-microservice/backups"
+
 def backup(url):
   # get current date
   curr_date = date.today().strftime("%Y-%m-%d")
@@ -21,16 +23,18 @@ def backup(url):
   response_str = response.read().decode("utf")
 
   # create directory w/ current date if not present already
-  dir_name = curr_date
+  dir_name = os.path.join(ABSOLUTE_PATH, curr_date)
   if not os.path.exists(dir_name):
     os.makedirs(dir_name)
 
   # save requested files to directory (overwrite if needed)
   file_name = url.split("/")[-1]
-  with open(os.path.join(dir_name, file_name), "w+") as f:
+  full_path = os.path.join(dir_name, file_name)
+  with open(full_path, "w+") as f:
+    print(f"Writing data for {curr_date} to {full_path}")
     f.write(response_str)
-
 
 if __name__ == "__main__":
   for url in [CONFIRMED_URL, DEATHS_URL]:
     backup(url)
+  print()
