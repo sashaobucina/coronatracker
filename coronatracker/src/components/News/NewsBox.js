a90bef182be169d1d0e12fcb022a7bac17754d1b
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-15 13:13:17 -0400

    Add support for keyword search in news

--- coronatracker/src/components/News/NewsBox.js
+++ coronatracker/src/components/News/NewsBox.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Pagination from "@material-ui/lab/Pagination";
 import NewsCard from "./NewsCard";
 import NewsSkeleton from "./NewsSkeleton";
+import TextSearch from "../Shared/TextSearch";
 import { PaginationItem } from "@material-ui/lab";
 import { withStyles } from "@material-ui/styles";
 
@@ -13,43 +14,52 @@ const StyledPaginationItem = withStyles({
     borderColor: "rgba(59, 186, 156, 0.5)",
     color: "#3BBA9C",
     "&:hover": {
-      opacity: 0.9
+      opacity: 0.9,
     },
     "&.Mui-selected": {
       color: "#3BBA9C",
       border: "1px solid #3BBA9C",
       backgroundColor: "rgba(59, 186, 156, 0.25)",
       "&:hover": {
-        backgroundColor: "rgba(59, 186, 156, 0.25)"
-      }
-    }
+        backgroundColor: "rgba(59, 186, 156, 0.25)",
+      },
+    },
   },
 })(PaginationItem);
 
 export default function NewsBox(props) {
-  const [ page, setPage ] = useState(1);
-  const [ pageCount, setPageCount ] = useState(1);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [pageCount, setPageCount] = useState(1);
   const { news } = props;
 
-  const matches = useMediaQuery('(min-width:600px)');
+  const matches = useMediaQuery("(min-width:600px)");
+
+  const filterNews = useCallback(
+    ({ description, title }) =>
+      (title && title.toLowerCase().includes(query)) ||
+      (description && description.toLowerCase().includes(query)),
+    [query]
+  );
 
   useEffect(() => {
-    setPageCount(Math.ceil(news.length / 10));
-  }, [news]);
+    setPageCount(Math.ceil(news.filter(filterNews).length / 10));
+    setPage(1);
+  }, [news, filterNews]);
 
   const handlePageChange = (_, value) => {
     setPage(value);
-  }
+  };
 
-  const getNewsCards = () => {
-    return news
+  const getNewsCards = () =>
+    news
+      .filter(filterNews)
       .slice((page - 1) * 10, (page - 1) * 10 + 10)
       .map((story, i) => (
         <Grid item key={i}>
           <NewsCard {...story} />
         </Grid>
-    ));
-  }
+      ));
 
   const emptyComponent = (
     <React.Fragment>
@@ -66,6 +76,9 @@ export default function NewsBox(props) {
 
   const newsComponent = (
     <React.Fragment>
+      <Grid item>
+        <TextSearch label="Search keyword" setQuery={setQuery} />
+      </Grid>
       {getNewsCards()}
       <Grid item>
         <Pagination
@@ -86,10 +99,7 @@ export default function NewsBox(props) {
 
   return (
     <Grid container direction="column" spacing={1}>
-      {news.length === 0
-        ? emptyComponent
-        : newsComponent
-      }
+      {news.length === 0 ? emptyComponent : newsComponent}
     </Grid>
   );
 }
