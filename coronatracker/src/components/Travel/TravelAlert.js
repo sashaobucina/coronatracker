c8133e076afe55aa9ee81cd6916b77c8f0ab5640
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-19 02:55:46 -0400

    Formatting code

--- coronatracker/src/components/Travel/TravelAlert.js
+++ coronatracker/src/components/Travel/TravelAlert.js
@@ -8,37 +8,37 @@ import {
   List,
   ListItem,
   ListItemText,
-  Typography
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyle = makeStyles({
   root: {
     background: "#373B52",
-    color: "#3BBA9C"
+    color: "#3BBA9C",
   },
   content: {
-    padding: "2px 16px 16px 16px"
+    padding: "2px 16px 16px 16px",
   },
   listItemText: {
-    color: "#7DB3A6"
+    color: "#7DB3A6",
   },
   header: {
-    padding: "16px 16px 2px 16px"
+    padding: "16px 16px 2px 16px",
   },
   cardFooter: {
-    padding: 10
+    padding: 10,
   },
   subheader: {
     color: "#3BBA9C",
-    fontSize: "0.85em"
+    fontSize: "0.85em",
   },
   title: {
     color: "#3BBA9C",
     fontSize: "1.2em",
     fontWeight: "bold",
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
 
 export default function TravelAlert(props) {
@@ -51,7 +51,10 @@ export default function TravelAlert(props) {
     <Grid container className={classes.cardFooter}>
       <Grid item>
         <Typography variant="caption">
-          Provided by <Link href="https://www.iatatravelcentre.com/international-travel-document-news/1580226297.htm">IATA</Link>
+          Provided by{" "}
+          <Linsk href="https://www.iatatravelcentre.com/international-travel-document-news/1580226297.htm">
+            IATA
+          </Link>
         </Typography>
       </Grid>
     </Grid>
@@ -60,28 +63,29 @@ export default function TravelAlert(props) {
   return (
     <Grid container direction="column">
       <Grid item>
-        <Card className={classes.root} >
+        <Card className={classes.root}>
           <CardHeader
             classes={{
               root: classes.header,
               subheader: classes.subheader,
-              title: classes.title
+              title: classes.title,
             }}
             title={`Travel Alert for ${country}`}
             subheader={subheader}
           />
           <CardContent className={classes.content}>
             <List dense>
-              {
-                description.split("\n").map((line, i) => (
-                  <ListItem key={i}>
-                    <ListItemText className={classes.listItemText} primary={line} />
-                  </ListItem>
-                ))
-              }
+              {description.split("\n").map((line, i) => (
+                <ListItem key={i}>
+                  <ListItemText
+                    className={classes.listItemText}
+                    primary={line}
+                  />
+                </ListItem>
+              ))}
             </List>
           </CardContent>
-          { supported ? footer : null }
+          {supported ? footer : null}
         </Card>
       </Grid>
     </Grid>
