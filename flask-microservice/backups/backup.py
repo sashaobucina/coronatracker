8ec44f7dfd7484516194bb8898ed8018fcadad82
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-20 15:55:24 -0400

    Add a latest directory that signifies latest data pull

--- flask-microservice/backups/backup.py
+++ flask-microservice/backups/backup.py
@@ -7,46 +7,63 @@ CONFIRMED_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/maste
 DEATHS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 RECOVERED_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
-ABSOLUTE_PATH = "/Users/sashaobucina/repos/coding_projects/coronatracker/flask-microservice/backups"
+ABSOLUTE_PATH = (
+    "/Users/sashaobucina/repos/coding_projects/coronatracker/flask-microservice/backups"
+)
 
 # get current date
 curr_date = date.today().strftime("%Y-%m-%d")
 
+
 def backup(url):
-  # get current time
-  curr_time = date.today().strftime("%Y-%m-%d %H:%M:%S") 
+    # get current time
+    curr_time = date.today().strftime("%Y-%m-%d %H:%M:%S")
 
-  # request data for confirmed and deaths
-  print(f"{curr_time}: Requesting data from {url}...")
-  response = request.urlopen(url)
+    # request data for confirmed and deaths
+    print(f"{curr_time}: Requesting data from {url}...")
+    response = request.urlopen(url)
 
-  if response.status != 200:
-    print(f"Was not able to access {url} - Status code {response.status}")
-    exit(1)
+    if response.status != 200:
+        print(f"Was not able to access {url} - Status code {response.status}")
+        exit(1)
 
-  response_str = response.read().decode("utf")
+    response_str = response.read().decode("utf")
 
-  # create directory w/ current date if not present already
-  dir_name = os.path.join(ABSOLUTE_PATH, curr_date)
-  if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+    # create directory signifying latest data pull
+    latest_dir = os.path.join(ABSOLUTE_PATH, "latest")
+    if not os.path.exists(latest_dir):
+        os.makedirs(latest_dir)
 
-  # save requested files to directory (overwrite if needed)
-  file_name = url.split("/")[-1]
-  full_path = os.path.join(dir_name, file_name)
-  with open(full_path, "w+") as f:
-    print(f"Writing data for {curr_date} to {full_path}")
-    f.write(response_str)
+    # create directory w/ current date if not present already
+    dir_name = os.path.join(ABSOLUTE_PATH, curr_date)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    # save requested files to both directories (overwrite if needed)
+    file_name = url.split("/")[-1]
+    full_path = os.path.join(dir_name, file_name)
+    full_latest_path = os.path.join(latest_dir, file_name)
+    with open(full_path, "w+") as f, open(full_latest_path, "w+") as latest_f:
+        print(f"Writing data for {curr_date} to {full_path} and {full_latest_path}")
+        f.write(response_str)
+        latest_f.write(response_str)
 
 if __name__ == "__main__":
-  for url in [CONFIRMED_URL, DEATHS_URL, RECOVERED_URL]:
-    backup(url)
+    for url in [CONFIRMED_URL, DEATHS_URL, RECOVERED_URL]:
+        backup(url)
 
-  # wrote to all files successfully, add the changes using git
-  try:
-    subprocess.run(["git", "-C", ABSOLUTE_PATH, "add", curr_date], check=True)
-    subprocess.run(["git", "-C", ABSOLUTE_PATH, "commit", "-m", f"Add backup for {curr_date}"], check=True)
-  except subprocess.CalledProcessError as grepexc:
-    print(f"Exited with non-zero return code {grepexc.returncode}: {grepexc.output}")
+    # wrote to all files successfully, add the changes using git
+    try:
+        subprocess.run(["git", "-C", ABSOLUTE_PATH, "add", curr_date], check=True)
+        subprocess.run(["git", "-C", ABSOLUTE_PATH, "add", "latest"], check=True)
+        subprocess.run(
+            ["git", "-C", ABSOLUTE_PATH, "commit", "-m", f"Add backup for {curr_date}"],
+            check=True,
+        )
+    except subprocess.CalledProcessError as grepexc:
+        print(
+            f"Exited with non-zero return code {grepexc.returncode}: {grepexc.output}"
+        )
 
-  print()
+    print()
+
